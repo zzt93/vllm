e1ad58fe37fa5e5e14cebc0e649cfe012a7b7020
凌煦 <wengang.cwg@antgroup.com>
2025-12-14 16:52:18 +0800

    PullRequest: 489 [Perf][MM] Use triton_mrope to avoid copy operations on AICPU
    Merge branch wengang/optimize-mrope of git@code.alipay.com:Theta/vllm.git into dev-v0.11.0.1111
    https://code.alipay.com/Theta/vllm/pull_requests/489
    
    Reviewed-by: 郝坤 <hk02335263@antgroup.com>
    
    * [Perf][MM] Use triton_mrope to avoid copy operations on AI CPU

--- vllm/model_executor/layers/rotary_embedding/mrope.py
+++ vllm/model_executor/layers/rotary_embedding/mrope.py
@@ -276,50 +276,7 @@ class MRotaryEmbedding(RotaryEmbeddingBase):
             query: [num_tokens, num_heads * head_size]
             key: [num_tokens, num_kv_heads * head_size]
         """
-        assert positions.ndim == 1 or positions.ndim == 2
-        assert key is not None
-
-        cos_sin_cache = self._match_cos_sin_cache_dtype(query)
-        num_tokens = positions.shape[-1]
-        cos_sin = cos_sin_cache[positions]
-        cos, sin = cos_sin.chunk(2, dim=-1)
-        if positions.ndim == 2:
-            assert self.mrope_section
-            if self.mrope_interleaved:
-                cos = apply_interleaved_rope(cos, self.mrope_section)
-                sin = apply_interleaved_rope(sin, self.mrope_section)
-            else:
-                cos = torch.cat(
-                    [m[i] for i, m in enumerate(cos.split(self.mrope_section, dim=-1))],
-                    dim=-1,
-                )
-                sin = torch.cat(
-                    [m[i] for i, m in enumerate(sin.split(self.mrope_section, dim=-1))],
-                    dim=-1,
-                )
-
-        query_shape = query.shape
-        query = query.view(num_tokens, -1, self.head_size)
-        query_rot = query[..., : self.rotary_dim]
-        query_pass = query[..., self.rotary_dim :]
-        query_rot = self.apply_rotary_emb.forward_native(
-            query_rot,
-            cos,
-            sin,
-        )
-        query = torch.cat((query_rot, query_pass), dim=-1).reshape(query_shape)
-
-        key_shape = key.shape
-        key = key.view(num_tokens, -1, self.head_size)
-        key_rot = key[..., : self.rotary_dim]
-        key_pass = key[..., self.rotary_dim :]
-        key_rot = self.apply_rotary_emb.forward_native(
-            key_rot,
-            cos,
-            sin,
-        )
-        key = torch.cat((key_rot, key_pass), dim=-1).reshape(key_shape)
-        return query, key
+        return self.forward_cuda(positions, query, key, offsets)
 
     def forward_cuda(
         self,
